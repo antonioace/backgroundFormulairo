0d3731e4b240a143f91bee09a6751f9788b73784
Antonio <antonioac@ufps.edu.co>
2022-11-14 23:23:30 -0500

    primer comit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      display:"flex",
+      background:"green"
+    }}>
+      <div className='contenido-izquierda'>
+      <div className='contenido-circulos'>
+        <div className='circulo-1'></div>
+        <div className='circulo-2'></div>
+        <div className='circulo-3'></div>
+      </div>
+      </div>
+   
+      <div className='contenido-derecha'>
+
+      </div>
+
     </div>
   );
 }
